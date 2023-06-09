e98c51e5066b37bccd21a97f4be655f0c2a7d604
ezemg <ezequielmatiasgr@gmail.com>
2023-06-09 08:30:30 -0300

    First commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    inputValue.trim().length > 1 && onNewCategory(inputValue.trim());
+    setInputValue('');
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
